5257d79889825f7be672b9e7b243c85d36260bb9
PaulaBanks <paulawambui@gmail.com>
2020-12-09 17:07:44 +0300

    9th Dec

--- force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
+++ force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
@@ -1,5 +1,24 @@
-import { LightningElement, api } from 'lwc';
-
+import { LightningElement, api, wire, track } from 'lwc';
+import sendSingleEmailLWC from '@salesforce/apex/SendSingleVFEmail.sendSingleEmailLWC';
 export default class SendEmailLWC extends LightningElement {
     @api flexipageRegionWidth = 'LARGE';
+    @api recordId;
+    @track error;
+    @track accounts;
+
+   handleSend(){
+    console.log('I got it!' + this.recordId);
+    sendSingleEmailLWC(this.recordId)
+        .then(result => {
+            this.accounts = result;
+        })
+        .catch(err => {
+            this.error = err;
+            console.log('Error '+ err);
+        })
+   }
+       
+   
+    
+    
 }
