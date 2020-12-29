5c53138369b2437c42ed227f44c00611f2dbf40d
Paula Banks <paulawambui@gmail.com>
2020-12-29 12:56:26 +0300

    Before Custom meta data

--- force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
+++ force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
@@ -1,20 +1,64 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import sendSingleEmailLWC from '@salesforce/apex/SendSingleVFEmail.sendSingleEmailLWC';
+import getInvoiceDetails from '@salesforce/apex/GetInvoiceDetails.getInvoiceDetails';
 export default class SendEmailLWC extends LightningElement {
     @api flexipageRegionWidth = 'LARGE';
     @api recordId;
     @track error;
-    @track accounts;
+    @track hasRendered = true;
+    
+    
+    invoiceNum = '';
+    contactEmail = '';
+    contactName = '';
+    subject = '';
+    body = '';
+    
+    
+    
+    renderedCallback(){
 
+        if(this.hasRendered){
+        console.log('The Id is ' + this.recordId);
+        this.hasRendered = false;
+
+        getInvoiceDetails({accID: this.recordId})
+                .then(result => {
+                    console.log('Results are ' + JSON.stringify(result));
+                    
+                    this.contactEmail = result.Email;
+                    this.invoiceNum = result.Invoice;
+                    this.contactName = result.Contact;
+                    console.log('Email ' + this.contactEmail);
+                    console.log('Contact ' + this.contactName);
+                    console.log('Invoice Number ' + this.invoiceNum);
+                    this.subject = 'Attn: '+ this.contactName + ' for Invoice ' + this.invoiceNum;
+                    this.body = 'Find the attached invoice. \n';
+                    this.error = undefined;
+                })
+                .catch(err => {
+                    console.log('Error found '+ err);
+                    this.error = err;
+                    
+                })
+   
+        }
+    }
+    handleClick(){
+        
+        const editor = this.template.querySelector('lightning-input-rich-text');
+        editor.setRangeText('');
+    }
+    
+    
    handleSend(){
-    console.log('I got it!' + this.recordId);
-    sendSingleEmailLWC(this.recordId)
+        sendSingleEmailLWC({acc: this.recordId})
         .then(result => {
-            this.accounts = result;
+               console.log('Result is '+ JSON.stringify(result));
         })
         .catch(err => {
-            this.error = err;
-            console.log('Error '+ err);
+            this.error = err;  
+            console.log('Error occured '+ err.body.message);
         })
    }
        
