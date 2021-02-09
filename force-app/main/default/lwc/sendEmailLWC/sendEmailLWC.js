02e4c402b27fb9012b086753c6b823fc053264cf
PaulaBnks <paulawambui@gmail.com>
2021-02-09 19:05:34 +0300

    After creating sample outbound email handler

--- force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
+++ force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
@@ -41,7 +41,7 @@ export default class SendEmailLWC extends LightningElement {
           console.log("Invoice Number " + this.invoiceNum);
         })
         .catch((err) => {
-          console.log("Error found " + err.body.message);
+          console.log("Error occured " + err.body.message);
           this.error = err;
         });
     }
